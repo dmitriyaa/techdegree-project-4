60ebf9932de9cd4ac00ecc73891f42b3d9978d1b
Dmitriy An <an.dmitriy01@gmail.com>
2017-05-15 03:56:35 +0200

    removed case sensitivity

--- js/searchbox.js
+++ js/searchbox.js
@@ -1,9 +1,9 @@
 $("input[name='search']").on("keyup", function() {
-   var searchInput = this.value;
+   var searchInput = this.value.toLowerCase();
 
    $(".gallery li").each(function(i) {
       var caption = $(this).children().attr("data-title");
-      var pureCaption = $(caption).text();
+      var pureCaption = $(caption).text().toLowerCase();
 
       if (pureCaption.indexOf(searchInput) > -1) {
          $(this).css('display', 'initial');
